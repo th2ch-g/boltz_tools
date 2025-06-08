cc75d973aee3e8d76d11648d7c08327607b0094e
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-08 23:05:10 +0900

    Initialize project with basic structure and dependency management.

---
+++ boltztools/cli.py
@@ -0,0 +1,48 @@
+import argparse
+import sys
+
+from .paeplot import PaePlot
+from .logger import generate_logger
+
+
+LOGGER = generate_logger(__name__)
+
+
+def cli() -> None:
+    # make parser
+    parser = argparse.ArgumentParser(
+        description=(
+            "Toolkit for boltz(Biomolecular interaction models) input and output files"
+        )
+    )
+
+    # subcommands
+    subparsers = parser.add_subparsers()
+
+    # paeplot
+    parser_paeplot = subparsers.add_parser("paeplot", help="paeplot mode")
+    parser_paeplot.add_argument("-i", "--input", help="Input directory containing the predicted models", type=str, required=True)
+    parser_paeplot.add_argument("-o", "--output", help="Suffix of the output plot files, e.g. foo_pae.png", default="", type=str)
+    parser_paeplot.add_argument("-c", "--cmap", help="Currently only 'bwr' and 'Greens_r' are supported", choices=["bwr", "Greens_r"], default="bwr", type=str)
+    parser_paeplot.add_argument("-t", "--title", help="Title of the plot", default="", type=str)
+    parser_paeplot.add_argument("-a", "--all", help="Plot all PAEs. Default is to plot only the best PAE", action="store_true")
+    parser_paeplot.add_argument("--dpi", help="DPI of the output plot. Default is 100. but 300 is recommeneded", default=100, type=int)
+
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        LOGGER.error(f"use {sys.argv[0]} --help")
+        sys.exit(1)
+
+    LOGGER.info(f"{sys.argv[1]} called")
+    if sys.argv[1] == "paeplot":
+        paeplot = PaePlot(args=args)
+        paeplot.run()
+
+
+    LOGGER.info(f"{sys.argv[1]} finished")
+
+
+
+
---
+++ boltztools/paeplot.py
@@ -0,0 +1,56 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
+from .logger import generate_logger
+
+matplotlib.use("Agg")
+
+
+LOGGER = generate_logger(__name__)
+
+class PaePlot:
+    args: ArgumentParser
+
+    def __init__(self, args):
+        self.args = args
+
+    def plot_all_paes(self):
+        for input_file in Path(f"{self.args.input}/predictions/").glob("*"):
+            for pae_npz in Path(f"{input_file}/").glob("pae_*_model_*.npz"):
+                LOGGER.info(f"Plotting {pae_npz}")
+                self.plot_pae(pae_npz)
+
+    def plot_best_pae(self):
+        for input_file in Path(f"{self.args.input}/predictions/").glob("*"):
+            for pae_npz in Path(f"{input_file}/").glob("pae_*_model_0.npz"):
+                LOGGER.info(f"Plotting {pae_npz}")
+                self.plot_pae(pae_npz)
+
+    def plot_pae(self, pae_npz):
+        with np.load(pae_npz) as data:
+            plt.figure(figsize=(8, 6))
+            im = plt.imshow(data['pae'], cmap=self.args.cmap, interpolation='nearest')
+            plt.colorbar(im, label='Predicted Aligned Error [Å]')
+            plt.xlabel('Residue Index')
+            plt.ylabel('Residue Index')
+            plt.title(f"{self.args.title}")
+            plt.tight_layout()
+            plt.savefig(f"{pae_npz.parent}/{pae_npz.stem}{self.args.output}.png", dpi=self.args.dpi)
+            LOGGER.info(f"Saved {pae_npz.parent}/{pae_npz.stem}{self.args.output}.png")
+            plt.clf()
+            plt.close()
+
+    def run(self):
+        LOGGER.info(f"{self.args=}")
+
+        if not Path(self.args.input).is_dir():
+            FileNotFoundError(f"{self.args.input} is not a directory")
+
+        if self.args.all:
+            self.plot_all_paes()
+        else:
+            self.plot_best_pae()
+
