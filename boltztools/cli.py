1301b267cf929d263a76d3186fc8ce7e754532fd
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-08 23:13:13 +0900

    Add PDE plotting functionality

--- boltztools/cli.py
+++ boltztools/cli.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 
 from .paeplot import PaePlot
+from .pdeplot import PdePlot
 from .logger import generate_logger
 
 
@@ -28,6 +29,14 @@ def cli() -> None:
     parser_paeplot.add_argument("-a", "--all", help="Plot all PAEs. Default is to plot only the best PAE", action="store_true")
     parser_paeplot.add_argument("--dpi", help="DPI of the output plot. Default is 100. but 300 is recommeneded", default=100, type=int)
 
+    # pdeplot
+    parser_pdeplot = subparsers.add_parser("pdeplot", help="pdeplot mode")
+    parser_pdeplot.add_argument("-i", "--input", help="Input directory containing the predicted models", type=str, required=True)
+    parser_pdeplot.add_argument("-o", "--output", help="Suffix of the output plot files, e.g. foo_pde.png", default="", type=str)
+    parser_pdeplot.add_argument("-c", "--cmap", help="Currently only 'bwr' and 'Greens_r' are supported", choices=["bwr", "Greens_r"], default="bwr", type=str)
+    parser_pdeplot.add_argument("-t", "--title", help="Title of the plot", default="", type=str)
+    parser_pdeplot.add_argument("-a", "--all", help="Plot all pdes. Default is to plot only the best pde", action="store_true")
+    parser_pdeplot.add_argument("--dpi", help="DPI of the output plot. Default is 100. but 300 is recommeneded", default=100, type=int)
 
     args = parser.parse_args()
 
@@ -39,6 +48,9 @@ def cli() -> None:
     if sys.argv[1] == "paeplot":
         paeplot = PaePlot(args=args)
         paeplot.run()
+    if sys.argv[1] == "pdeplot":
+        pdeplot = PdePlot(args=args)
+        pdeplot.run()
 
 
     LOGGER.info(f"{sys.argv[1]} finished")
---
+++ boltztools/pdeplot.py
@@ -0,0 +1,56 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
+from .logger import generate_logger
+
+matplotlib.use("Agg")
+
+
+LOGGER = generate_logger(__name__)
+
+class PdePlot:
+    args: ArgumentParser
+
+    def __init__(self, args):
+        self.args = args
+
+    def plot_all_pdes(self):
+        for input_file in Path(f"{self.args.input}/predictions/").glob("*"):
+            for pde_npz in Path(f"{input_file}/").glob("pde_*_model_*.npz"):
+                LOGGER.info(f"Plotting {pde_npz}")
+                self.plot_pde(pde_npz)
+
+    def plot_best_pde(self):
+        for input_file in Path(f"{self.args.input}/predictions/").glob("*"):
+            for pde_npz in Path(f"{input_file}/").glob("pde_*_model_0.npz"):
+                LOGGER.info(f"Plotting {pde_npz}")
+                self.plot_pde(pde_npz)
+
+    def plot_pde(self, pde_npz):
+        with np.load(pde_npz) as data:
+            plt.figure(figsize=(8, 6))
+            im = plt.imshow(data['pde'], cmap=self.args.cmap, interpolation='nearest')
+            plt.colorbar(im, label='Predicted Aligned Error [Å]')
+            plt.xlabel('Residue Index')
+            plt.ylabel('Residue Index')
+            plt.title(f"{self.args.title}")
+            plt.tight_layout()
+            plt.savefig(f"{pde_npz.parent}/{pde_npz.stem}{self.args.output}.png", dpi=self.args.dpi)
+            LOGGER.info(f"Saved {pde_npz.parent}/{pde_npz.stem}{self.args.output}.png")
+            plt.clf()
+            plt.close()
+
+    def run(self):
+        LOGGER.info(f"{self.args=}")
+
+        if not Path(self.args.input).is_dir():
+            FileNotFoundError(f"{self.args.input} is not a directory")
+
+        if self.args.all:
+            self.plot_all_pdes()
+        else:
+            self.plot_best_pde()
+
